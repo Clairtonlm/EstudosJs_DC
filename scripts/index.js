3d6cb7faa74990b26dd6841ec0de422321c08fc9
clairtonlm <clairtonlm@gmail.com>
2022-10-22 00:53:24 -0300

    Addcionand Funcionalides

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,20 @@
 /* Função que marca o jogo*/
 let gammer = "X"
+
 function winner(){
+    let elements = document.getElementById("tabuleiro"). children;
+    if( elements[0].innerText === elements[1].innerTexts && 
+        elements[1].innerText === elements[2].innerText && 
+        elements[0].innerText.length !== 0
+        ){
+           
+           elements[0].style="background-color: blue;";
+           elements[1].style="background-color: blue;";
+           elements[2].style="background-color: blue;";
+            
+
+        }
+        let x = 1;
 
 }
 function mark(element){//mark é uma função
