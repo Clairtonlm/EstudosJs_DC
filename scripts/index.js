b2e2204838ac8751fdbf0db8daed65e246bc2932
clairtonlm <clairtonlm@gmil.com>
2022-10-26 21:23:13 -0300

    Continuando jogo da velha
    Estamos atualizando o jogo da veia

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,78 @@
 /* Função que marca o jogo*/
-let gammer = "X"
+let gammer = "X";
+let finished = false;
+
 
 function winner(){
-    let elements = document.getElementById("tabuleiro"). children;
-    if( elements[0].innerText === elements[1].innerTexts && 
-        elements[1].innerText === elements[2].innerText && 
+    let elements = document.getElementById("tabuleiro").children;
+    if( elements[0].innerText === elements[1].innerTexts && //OS texto dos elementos sao iguai?
+        elements[1].innerText === elements[2].innerText && // innerText quer dizer o texto é igual?
         elements[0].innerText.length !== 0
         ){
            
-           elements[0].style="background-color: blue;";
-           elements[1].style="background-color: blue;";
-           elements[2].style="background-color: blue;";
-            
+           elements[0].style="background-color: blue";
+           elements[1].style="background-color: blue";
+           elements[2].style="background-color: blue"; 
+           return  true;
 
         }
-        let x = 1;
+
+        if( 
+        elements[3].innerText === elements[4].innerTexts && //OS texto dos elementos sao iguai?
+        elements[4].innerText === elements[5].innerText && 
+        elements[3].innerText.length !== 3
+        ){
+           
+           elements[3].style="background-color: blue";
+           elements[4].style="background-color: blue";
+           elements[5].style="background-color: blue";
+           return  true;
+        }
+        if( 
+        elements[6].innerText === elements[7].innerTexts && //OS texto dos elementos sao iguai?
+        elements[7].innerText === elements[8].innerText && 
+        elements[6].innerText.length !== 0
+        ){
+           
+           elements[6].style="background-color: blue";
+           elements[7].style="background-color: blue";
+           elements[8].style="background-color: blue";
+           return  true; 
+
+        }
+        if( 
+        elements[0].innerText === elements[4].innerTexts && //OS texto dos elementos sao iguai?
+        elements[4].innerText === elements[8].innerText && 
+        elements[0].innerText.length !== 0
+        ){
+           
+           elements[0].style="background-color: blue";
+           elements[4].style="background-color: blue";
+           elements[8].style="background-color: blue";
+           return  true; 
+
+        }
+        if(
+        elements[2].innerText === elements[4].innerTexts && //OS texto dos elementos sao iguai?
+        elements[4].innerText === elements[6].innerText && 
+        elements[2].innerText.length !== 0
+        ){
+           
+           elements[2].style="background-color: blue";
+           elements[4].style="background-color: blue";
+           elements[6].style="background-color: blue"; 
+           return  true;
+
+        }
+        
 
 }
+
 function mark(element){//mark é uma função
 
-   if(element.innerText.length === 0){
+   if(element.innerText.length === 0 && !finished ){
     element.innerText= gammer;// innerTex é uma propriedade
+    let resp = winner();
 
     winner();
 
@@ -30,5 +82,5 @@ function mark(element){//mark é uma função
         gammer = "X";
     }
 
-}
+    }
 }
